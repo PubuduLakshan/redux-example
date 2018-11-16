48759995ffa6dd4ad611e6958d97b9c07169b9bc
Pubudu Kulasinghe <pubudu_k@3logixs.lk>
2018-11-16 09:49:48 +0530

    initial commit

---
+++ src/components/postform.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class PostForm extends Component{
+
+    render(){
+
+        return(
+
+            <div className="h1">PostForm</div>
+        );
+
+    }
+}
